3930335a79f97af00dce301342873be39ba76d5b
henrique <henriquehvisa@gmail.com>
2020-05-05 22:17:53 -0300

    Implement SchoolService

---
+++ src/Eleva.Domain/Interfaces/ISchoolService.cs
@@ -0,0 +1,14 @@
+﻿using Eleva.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eleva.Domain.Interfaces
+{
+    public interface ISchoolService
+    {
+        IEnumerable<SchoolDTO> GetAll();
+        SchoolDTO GetById(Guid id);
+       
+    }
+}
---
+++ src/Eleva.Domain/Services/SchoolService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Eleva.Application.DTO;
+using Eleva.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eleva.Domain.Services
+{
+    public class SchoolService : ISchoolService
+    {
+        private readonly IMapper _mapper;
+        private readonly ISchoolRepository _schoolRepository;
+
+        public SchoolService(IMapper mapper, ISchoolRepository schoolRepository)
+        {
+            _mapper = mapper;
+            _schoolRepository = schoolRepository;
+        }
+
+        public IEnumerable<SchoolDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<SchoolDTO>>(_schoolRepository.GetAll()); 
+        }
+
+        public SchoolDTO GetById(Guid id)
+        {
+            return _mapper.Map<SchoolDTO>(_schoolRepository.GetById(id));
+        }
+    }
+}
